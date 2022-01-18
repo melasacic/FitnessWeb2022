1f94654366928d0d671f4a37f079d9d18d2c8892
Mela Sacic <mela.sacic@stu.ibu.edu.ba>
2022-01-18 11:51:44 +0100

    add parsing of query parameters

--- api/index.php
+++ api/index.php
@@ -6,11 +6,19 @@ error_reporting(E_ALL);
 require_once dirname(__FILE__).'/../vendor/autoload.php';
 require_once dirname(__FILE__).'/dao/ClientDao.class.php';
 
+// utility function for reading query parameters from URL
+Flight::map('query', function($name, $default_value = NULL){
+  $request = Flight::request();
+  $query_param = @$request->query->getData()[$name];
+  $query_param = $query_param ? $query_param : $default_value;
+  return $query_param;
+});
+
+// register DAO layer
 Flight::register('clientDao', 'ClientDao');
 
+// include all routes
 require_once dirname(__FILE__)."/routes/clients.php";
 
-
-
-
+Flight::start();
 ?>
--- api/routes/clients.php
+++ api/routes/clients.php
@@ -1,28 +1,31 @@
 <?php
+
 Flight::route('GET /clients', function(){
-    Flight::json(Flight::clientDao()->get_all(0, 10));
+  $offset = Flight::query('offset', 0);
+  $limit = Flight::query('limit', 10);
+
+  Flight::json(Flight::clientDao()->get_all($offset, $limit));
 });
 
 Flight::route('GET /clients/@id', function($id){
-    Flight::json(Flight::clientDao()->get_by_id($id));
+  Flight::json(Flight::clientDao()->get_by_id($id));
 });
 
 // route for posting new client and saving him to DB
 // kada posaljem request podatci koje sam unjela se spase u $data
 Flight::route('POST /clients', function(){
-    $request = Flight::request()->data->getData();
-    Flight::json(Flight::clientDao()->add($data));
+  $request = Flight::request()->data->getData();
+  Flight::json(Flight::clientDao()->add($data));
 });
 
 // getting the account that we are updateing
 Flight::route('PUT /clients/@id', function($id){
-    $request = Flight::request();
-    $data = $request->data->getData();
-    Flight::clientDao()->update($id, $data);
-    $client = Flight::clientDao()->get_by_id($id);
-    Flight::request($client);
+  $request = Flight::request();
+  $data = $request->data->getData();
+  Flight::clientDao()->update($id, $data);
+  $client = Flight::clientDao()->get_by_id($id);
+  Flight::request($client);
 });
-
 
 Flight::start();
  ?>
