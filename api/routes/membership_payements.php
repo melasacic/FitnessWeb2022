831ad122aef7ae827f5815eb81409a04bf384fd9
Mela Sacic <mela.sacic@stu.ibu.edu.ba>
2022-01-19 15:48:36 +0100

    get and update membership_payements

--- api/routes/membership_payements.php
+++ api/routes/membership_payements.php
@@ -13,13 +13,21 @@ Flight::route('GET /membership_payements', function(){
   Flight::json(Flight::membershipPayementService()->get_membership_payements($client_id, $offset, $limit, $order));
 });
 
-
+Flight::route('GET /membership_payements/@id', function($id){
+  Flight::json(Flight::membershipPayementService()->get_by_id($id));
+});
 // route for posting new client and saving him to DB
 // kada posaljem request podatci koje sam unjela se spase u $data
 Flight::route('POST /membership_payements', function(){
   $data = Flight::request()->data->getData();
   Flight::json(Flight::membershipPayementService()->add($data));
 });
+
+Flight::route('PUT /membership_payements/@id', function($id){
+  $data = Flight::request()->data->getData();
+  Flight::json(Flight::membershipPayementService()->update($id, $data));
+});
+
 
 Flight::start();
 ?>
