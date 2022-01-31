dfbfd8a452f8a3ddf4d1f5d6fd25414aaa89c75b
Mela Sacic <mela.sacic@stu.ibu.edu.ba>
2022-01-31 16:44:05 +0100

    other middleware option

--- api/routes/middleware.php
+++ api/routes/middleware.php
@@ -1,16 +1,17 @@
 <?php
-
+/*
+FILTER BASED MIDDLEWARE
 Flight::before('start', function(&$params, &$output){
 
-  /* user routes are publically avaliable*/
+  // user routes are publically avaliable
   if(str_starts_with(Flight::request()->url, '/users/')) return TRUE;
 
- /* all other methods require authentification*/
+ // all other methods require authentification
   $headers = getallheaders();
   $token = @$headers['Authentication'];
-  /* decode token and transform into class obj*/
+  // decode token and transform into class obj
   try {
-    /*if token is successfuly dedoced we can perform this action*/
+    //if token is successfuly dedoced we can perform this action
     $decoded = (array)\Firebase\JWT\JWT::decode($token, "JWT SECRET", ['HS256']);
     Flight::set('user', $decoded);
     // ADMIN -> create set of routes /admin/something
@@ -18,10 +19,35 @@ Flight::before('start', function(&$params, &$output){
     // USER_READ_ONLY -> block POST and PUT methods
     return TRUE;
   } catch (\Exception $e) {
-    /*if not fail*/
+    //if not fail
     Flight::json(["message" => $e->getMessage()], 401);
-    /*return FALSE;*/
+    //return FALSE;
     die;
   }
-});
+});*/
+
+// ROUTE BASED MIDDLEWARE -> with thisn option i can create multiple middlewares
+Flight::route('*', function(){
+  // user routes are publically avaliable
+  if(str_starts_with(Flight::request()->url, '/users/')) return TRUE;
+
+ // all other methods require authentification
+  $headers = getallheaders();
+  $token = @$headers['Authentication'];
+  // decode token and transform into class obj
+  try {
+    //if token is successfuly dedoced we can perform this action
+    $decoded = (array)\Firebase\JWT\JWT::decode($token, "JWT SECRET", ['HS256']);
+    Flight::set('user', $decoded);
+    // ADMIN -> create set of routes /admin/something
+    // USER -> set of routes of regular users
+    // USER_READ_ONLY -> block POST and PUT methods
+    return TRUE;
+  } catch (\Exception $e) {
+    //if not fail
+    Flight::json(["message" => $e->getMessage()], 401);
+    //return FALSE;
+    die;
+  }
+})
  ?>
