dd489df46f59e95234a04e5d39b919fe91b03fe3
Mela Sacic <mela.sacic@stu.ibu.edu.ba>
2022-02-02 11:27:48 +0100

    some update - still does not work

--- api/routes/membership_payements.php
+++ api/routes/membership_payements.php
@@ -2,7 +2,7 @@
 
 // OVE RUTE SU PRESENTATION LAYER I ONE ZOVU BUSSINES LAYER STO SU NAMA SERVICES
 
-Flight::route('GET /membership_payements', function(){
+Flight::route('GET /membership_payement', function(){
 
   $client_id = Flight::query('client_id');
   $offset = Flight::query('offset', 0);
